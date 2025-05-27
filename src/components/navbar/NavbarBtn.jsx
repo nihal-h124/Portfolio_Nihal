ca3e6ebc69660eea16daf4fb1fb3410f46fdf02f
nihal-h <f.socy10@gmail.com>
2025-05-27 02:13:29 +0200

    Fixed onClick for Hire Me button

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { PiArrowCircleUpRightFill } from "react-icons/pi";
 
+const links = [
+  { link: "Kontakt", section: "contact" }
+];
+
 const NavbarBtn = () => {
   return (
-    <button className='px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow'>
+    <button onClick={() => {
+      const section = links.find(link => link.link === "Kontakt");
+      if (section) {
+        const element = document.getElementById(section.section);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    }} className='px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow'>
      Hire Me
      <div className='sm:hidden md:block'>
         <PiArrowCircleUpRightFill />
